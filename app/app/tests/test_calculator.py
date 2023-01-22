586b254e214324c99d5cebefbcc816b2ab85eb65
Alves Vitor <Vtor Alves>
2023-01-21 21:51:31 -0300

    adding db to docker compose

--- app/app/tests/test_calculator.py
+++ app/app/tests/test_calculator.py
@@ -1,13 +1,14 @@
 from django.test import SimpleTestCase
-# from .calculator import add
-from .calculator import add
-# Create your tests here.
+from ..calculator import add, subtract
 
 
 class TestClass(SimpleTestCase):
     """Test making a list unique"""
 
-    def test_make_test(self):
-        # calculator.add
+    def test_add(self):
         result = add(1, 2)
         self.assertEqual(result, 3)
+
+    def test_subtract(self):
+        result = subtract(3, 2)
+        self.assertEqual(result, 1)
